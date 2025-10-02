8a7d1eeebb349eb173056f0296cfbb28933926a2
ltcevil <84654599+ltcevil@users.noreply.github.com>
2025-10-02 12:34:07 +0200

    Update telegram_send/__init__.py for feature: Analyze The Code And Tell Me Options For New Features

--- telegram_send/__init__.py
+++ telegram_send/__init__.py
@@ -1,5 +1,9 @@
 from .version import __version__
 from .telegram_send import configure, send
+from .message_processor import process_json_message
+from .button_handler import send_with_buttons
+from .profile_manager import ProfileManager
+from .chat_manager import ChatManager
 
 
-__all__ = ["configure", "send"]
+__all__ = ["configure", "send", "process_json_message", "send_with_buttons", "ProfileManager", "ChatManager"]
